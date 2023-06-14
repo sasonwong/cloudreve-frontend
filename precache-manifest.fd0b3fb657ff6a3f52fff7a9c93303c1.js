527d0e621baf55848b0879d579566a85a5c200ab
Sason Wong <8159419+sasonwong@users.noreply.github.com>
2023-06-14 21:45:57 +0800

    Initial Commit
    Cloudreve frontend exported from ./cloudreve -eject

---
+++ precache-manifest.fd0b3fb657ff6a3f52fff7a9c93303c1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e52c73e8504c8a4a180effb45f301401",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30d313d719a59d67243f",
+    "url": "/static/css/6.57254ef2.chunk.css"
+  },
+  {
+    "revision": "9b7ecf22740f611bab42",
+    "url": "/static/js/10.a87bd80b.chunk.js"
+  },
+  {
+    "revision": "322f890d5946d94c3f11",
+    "url": "/static/js/11.eb10979a.chunk.js"
+  },
+  {
+    "revision": "eef91b12ae88844bad81",
+    "url": "/static/js/12.dad7b62c.chunk.js"
+  },
+  {
+    "revision": "9c17c0884eb20b13c4ce",
+    "url": "/static/js/13.c2f9dc35.chunk.js"
+  },
+  {
+    "revision": "30d313d719a59d67243f",
+    "url": "/static/js/6.2cdf7034.chunk.js"
+  },
+  {
+    "revision": "1d25bcbc2653e202751e",
+    "url": "/static/js/7.f69642a2.chunk.js"
+  },
+  {
+    "revision": "20bfaf62f9ccc1e635e0",
+    "url": "/static/js/8.ebe589a9.chunk.js"
+  },
+  {
+    "revision": "bcc90efcda1982dc4b08",
+    "url": "/static/js/9.539305ba.chunk.js"
+  },
+  {
+    "revision": "61e2f24ebb3c438f7526",
+    "url": "/static/js/codeEditor.febe05e7.chunk.js"
+  },
+  {
+    "revision": "92dbae0628892514f765",
+    "url": "/static/js/main.b28185a0.chunk.js"
+  },
+  {
+    "revision": "96fbf22856295a097285",
+    "url": "/static/js/runtime-main.ae3eaca0.js"
+  },
+  {
+    "revision": "51c934afb61ee321ae0d9f05a4b432e9",
+    "url": "/static/media/getFetch.51c934af.cjs"
+  }
+]);
